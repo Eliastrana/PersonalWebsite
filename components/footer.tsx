89700bea24f3fa9271ad7cc8b521fcd27be3f5a7
eliastt <eliastt@stud.ntnu.no>
2023-12-22 23:45:47 +0100

    added minigame?

--- components/footer.tsx
+++ components/footer.tsx
@@ -13,15 +13,23 @@ const Footer = () => {
           </h3>
 
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+
             <a
-              href={"archive/archive.html"}
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+                href={"minigame/minigame.tsx"}
+                className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+            >
+              Play
+            </a>
+
+            <a
+                href={"archive/archive.html"}
+                className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               Archive
             </a>
             <a
-              href={`https://github.com/Eliastrana/nextjsportfolio`}
-              className="mx-3 font-bold hover:underline"
+                href={`https://github.com/Eliastrana/nextjsportfolio`}
+                className="mx-3 font-bold hover:underline"
             >
               Code for this site
             </a>
