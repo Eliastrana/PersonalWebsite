39b1f0459765049fe1ec584808dd27731875b989
eliastt <eliastt@stud.ntnu.no>
2023-12-24 10:25:15 +0100

    linking game to page

--- components/footer.tsx
+++ components/footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
 
             <a
-                href={"minigame/minigame.tsx"}
+                href={"minigame/minigame.html"}
                 className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               Play
