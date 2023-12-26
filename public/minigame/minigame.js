2a48235ab66eeeed0a2787e03c6632b7279367a2
eliastt <eliastt@stud.ntnu.no>
2023-12-26 15:01:45 +0100

    Polished the game, it is still kinda bad

--- public/minigame/minigame.js
+++ public/minigame/minigame.js
@@ -50,6 +50,8 @@ const obstacleSpeed = 3;
 let score = 0;
 let lastObstacleTopHalf = false;
 let gameStarted = false;
+let gameTimeRemaining = 100;
+
 
 // Global image and sound variables
 let skierImage = new Image();
@@ -58,7 +60,16 @@ const backgroundMusic = new Audio('background_music.wav'); // Replace with your 
 const pointGainSound = new Audio('getpoint.wav'); // Replace with your point gain sound path
 const pointLossSound = new Audio('loosepoint.wav'); // Replace with your point loss sound path
 
+
+const maxObstacles = 20; // Maximum number of obstacles allowed
+
+
 function spawnObstacle() {
+
+    if (obstacles.length >= maxObstacles) {
+        return; // Do not add more obstacles if the limit is reached
+    }
+
     const x = gameCanvas.width;
     let y;
     const safeZone = 60;
@@ -80,10 +91,10 @@ function updateScore(pointChange) {
     score += pointChange;
     document.getElementById('score').textContent = 'Score: ' + score;
 
-    if (score < 0) {
-        gameOver();
-        return;
-    }
+    // if (score < 0) {
+    //     gameOver();
+    //     return;
+    // }
 
     if (pointChange > 0) {
         pointGainSound.play();
@@ -128,10 +139,10 @@ function updateGame() {
 }
 
 function gameOver() {
-    alert("Game Over! Your final score is: " + score);
-    clearInterval(obstacleSpawnIntervalID);
+    alert("Time's up! Your final score is: " + score);
     gameStarted = false;
     document.getElementById('startButton').style.display = 'block';
+    document.getElementById('startButton').textContent = 'Play Again';
     obstacles = [];
     score = 0;
     backgroundMusic.pause();
@@ -151,16 +162,47 @@ function startGame() {
     }
 
     gameStarted = true;
+    score = 0; // Reset score
+    obstacles = []; // Clear obstacles
+    clearInterval(obstacleSpawnIntervalID); // Clear existing interval for spawning obstacles
+    gameTimeRemaining = 100; // Reset game timer
+
+    // Reset UI elements
+    document.getElementById('score').textContent = 'Score: ' + score;
+    document.getElementById('timer').textContent = 'Time: ' + gameTimeRemaining;
     document.getElementById('startButton').style.display = 'none';
 
+    // Reset and start the game timer
+    const gameTimer = setInterval(() => {
+        gameTimeRemaining -= 1;
+        document.getElementById('timer').textContent = 'Time: ' + gameTimeRemaining;
+
+        if (gameTimeRemaining <= 0) {
+            clearInterval(gameTimer);
+            gameOver();
+        }
+    }, 1000);
+
+    // Restart background music
+    backgroundMusic.pause();
+    backgroundMusic.currentTime = 0;
     backgroundMusic.loop = true;
     backgroundMusic.volume = 0.15;
     backgroundMusic.play();
 
+    // Reinitialize skier
     skier = new Skier(skierImage);
+
+    // Start spawning obstacles
     obstacleSpawnIntervalID = setInterval(spawnObstacle, 2000);
+
+    // Start the game loop
     updateGame();
 }
+
+
+
+
 
 // Set image sources
 skierImage.src = 'skier.png'; // Replace with the path to your skier image
