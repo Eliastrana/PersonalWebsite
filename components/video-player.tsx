ea63c619e24349d6e77437e95b57d6e66b283e5f
eliastrana <eliastrana@gmail.com>
2024-03-02 22:58:29 +0100

    again ;(

--- components/video-player.tsx
+++ components/video-player.tsx
@@ -10,15 +10,16 @@ interface VideoPlayerProps {
 const VideoPlayer: React.FC<VideoPlayerProps> = ({ videoUrl, title, undertitle }) => {
     return (
         <div className={`relative w-full overflow-hidden ${styles.videoContainer}`}>
+
             <video
                 className={`absolute top-0 left-0 right-0 bottom-0 object-cover ${styles.videoPlayer} ${styles.videoPlayerMobile}`}
                 src={videoUrl}
-                controls
                 autoPlay
                 loop
                 muted
                 playsInline
             ></video>
+
             <div className="absolute inset-0 flex flex-col justify-center items-center z-10">
                 <h1 className={`${styles.customFont} text-white mb-8 text-center`}>{title}</h1>
                 <h3 className={`${styles.customFontSmall} text-white text-center`}>{undertitle}</h3>
